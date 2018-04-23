a1c0f62c9815f1b502cc91af2a47df6976825694
Ifeanyichukwu Nwosu <ifeanyichukwu_nwosu@yahoo.com>
2018-04-22 19:41:45 -0600

    Update edit_famFile.py

--- edit_famFile.py
+++ edit_famFile.py
@@ -1,3 +1,5 @@
+#This script generates a new file that has only individuals with case or control information
+
 #! /usr/bin/python
 
 import sys
@@ -8,5 +10,5 @@ with open(sys.argv[1], "r") as readFile:
 	with open(sys.argv[2], "w") as writeFile:
 		for line in readFile:
 			line = line.strip().split(" ")
-			if "-9" in line:
+			if "-9" not in line:
 					writeFile.write(line[0] + "_" + line[1] + "\n")
